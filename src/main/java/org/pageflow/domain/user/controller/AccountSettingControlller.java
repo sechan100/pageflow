255373be6c21c561f9154281edca98f23603f8e9
sechan100 <sechan100@gmail.com>
2023-10-30 13:49:11 +0900

    feat: 계정 프로필 수정 페이지 구성
    계정 프로필 정보 수정 페이지를 구성, js로 input에 업로드한 파일의 preview를 보여주는 코드 작성

--- src/main/java/org/pageflow/domain/user/controller/AccountSettingControlller.java
+++ src/main/java/org/pageflow/domain/user/controller/AccountSettingControlller.java
@@ -1,13 +1,31 @@
 package org.pageflow.domain.user.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.pageflow.base.request.Rq;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountSettingControlller {
     
+    private final Rq rq;
+    
     @GetMapping("/account/settings")
-    public String accountSettings() {
+    public String accountSettings(Model model) {
+        
+        model.addAttribute("account", rq.getAccount());
+        
+        return "/user/account/settings";
+    }
+    
+    @PostMapping("/account/settings")
+    public String accountSettingsPost(Model model) {
+        
+        model.addAttribute("account", rq.getAccount());
+        
         return "/user/account/settings";
     }
 }

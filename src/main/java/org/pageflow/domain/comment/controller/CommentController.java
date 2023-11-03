1a762b96c2f86b0f7cf62f446a073992608fbedc
gsw05018 <gsw05018@naver.com>
2023-11-03 13:25:19 +0900

    feat : 댓글 수정 기능 구현

--- src/main/java/org/pageflow/domain/comment/controller/CommentController.java
+++ src/main/java/org/pageflow/domain/comment/controller/CommentController.java
@@ -4,16 +4,21 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.pageflow.domain.book.entity.Book;
 import org.pageflow.domain.book.service.BookService;
+import org.pageflow.domain.comment.entity.Comment;
 import org.pageflow.domain.comment.form.CommentForm;
 import org.pageflow.domain.comment.service.CommentService;
 import org.pageflow.domain.user.entity.Account;
 import org.pageflow.domain.user.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -37,6 +42,32 @@ public class CommentController {
         }
         this.commentService.create(book, commentForm.getContent(), author);
         return String.format("redirect:/book/detail/%s", id);
+    } //댓글 작성
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String modify(CommentForm commentForm, @PathVariable("id") Long id, Principal principal) {
+        Comment comment = this.commentService.getComment(id);
+        if (!comment.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        commentForm.setContent(comment.getContent());
+        return "/comment/comment_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String modify(@Valid CommentForm commentForm, BindingResult bindingResult,
+                               @PathVariable("id") Long id, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "comment//comment_form";
+        }
+        Comment comment = this.commentService.getComment(id);
+        if (!comment.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.commentService.modify(comment, commentForm.getContent());
+        return String.format("redirect:/book/detail/%s", comment.getBook().getId());
     }
 
 }
--- src/main/java/org/pageflow/domain/comment/service/CommentService.java
+++ src/main/java/org/pageflow/domain/comment/service/CommentService.java
@@ -1,11 +1,15 @@
 package org.pageflow.domain.comment.service;
 
 import lombok.RequiredArgsConstructor;
+import org.pageflow.domain.book.DataNotFoundException;
 import org.pageflow.domain.book.entity.Book;
 import org.pageflow.domain.comment.entity.Comment;
 import org.pageflow.domain.comment.repository.CommentRepository;
 import org.pageflow.domain.user.entity.Account;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +24,21 @@ public class CommentService {
                 .book(book)
                 .author(author)
                 .build();
+        this.commentRepository.save(comment);
+    }// 댓글 작성
+
+    public Comment getComment(Long id) {
+        Optional<Comment> comment = this.commentRepository.findById(id);
+        if (comment.isPresent()) {
+            return comment.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Comment comment, String content) {
+        comment.setContent(content);
+        comment.setModifyDate(LocalDateTime.now());
         this.commentRepository.save(comment);
     }
 }

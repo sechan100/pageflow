3ab808b1591a1457b56eac0f6ef1ee2eb03888dc
sechan100 <sechan100@gmail.com>
2023-11-11 11:18:50 +0900

    chore: 클래스 문서화 작업 추가
    Outline, SelectiveLISOptimizer 클래스 문서 추가

--- src/main/java/org/pageflow/domain/book/model/outline/Outline.java
+++ src/main/java/org/pageflow/domain/book/model/outline/Outline.java
@@ -8,6 +8,11 @@ import org.pageflow.domain.user.entity.Profile;
 
 import java.util.List;
 
+/**
+ * 무거운 전체 책 데이터를 담지 않고, 책의 개요 정보만을 담아서 전달하는 POJO DTO.
+ * 해당 클래스는 Book 엔티티와 대응된다고 볼 수 있고, Chapter와 Page 엔티티는 각각 {@link ChapterSummary}, {@link PageSummary}에 대응되어 담긴다.
+ * @author         : sechan
+ */
 @Data
 @Builder
 @AllArgsConstructor

4bbb4265738a5dc8603f4f78dffba3daafa11a76
sechan100 <sechan100@gmail.com>
2023-11-21 15:51:42 +0900

    feat: FE book_viewer 틀 개발

---
+++ src/main/react/book_viewer/src/components/nav/ProgressionBar.tsx
@@ -0,0 +1,24 @@
+
+
+
+
+
+interface ProgressionBarProps {
+  progressPercentage: number;
+
+}
+
+
+export default function ProgressionBar({progressPercentage} : ProgressionBarProps) {
+
+
+  return(
+    <div className="pb-5">
+
+      <div className="w-full bg-gray-300 rounded-full">
+        <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: `${progressPercentage}%`}}>{progressPercentage}%</div>
+      </div>
+
+    </div>
+  );
+}

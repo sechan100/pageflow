4f387cbb11bec66cfa07ef9001e55ded99a6daa5
hyeonjae1023 <hyeonjae1023111@gmail.com>
2023-11-29 11:16:06 +0900

    feat: details, view 연동 작업중

--- src/main/java/org/pageflow/domain/book/controller/BookViewController.java
+++ src/main/java/org/pageflow/domain/book/controller/BookViewController.java
@@ -1,14 +1,11 @@
 package org.pageflow.domain.book.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.pageflow.domain.book.model.outline.Outline;
 import org.pageflow.domain.book.service.BookService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 
 
@@ -23,9 +20,8 @@ public class BookViewController {
      * @return react book read form page
      */
     @GetMapping("/book/view/{bookId}")
-    public String bookViewForm( @PathVariable("bookId") Long bookId
-                                ) {
-        return "forward:/react/build/book_viewer/index.html";
+    public String bookViewForm( @PathVariable("bookId") Long bookId) {
+        return "forward:/react/build/book_viewer/index.html?bookId=" + bookId;
     }
 
 }

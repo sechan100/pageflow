e0d60c454592731741c0fa3955d99d56cad7612b
한인철 <gsw05018@naver.com>
2023-12-01 02:04:22 +0900

    feat: 비밀번호 ì재설정 페이지 구현

--- src/main/java/org/pageflow/domain/user/controller/PasswordFindController.java
+++ src/main/java/org/pageflow/domain/user/controller/PasswordFindController.java
@@ -10,8 +10,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Transactional
 public class PasswordFindController {
 
+    // 1차 회원 정보 입력
     @GetMapping("/find")
     public String passwordFind(){
         return "/user/account/password_Find";
+    }
+
+
+    // 2차 회원 정보 입력 후 비밀번호 재 설정
+    @GetMapping("/reset")
+    public String passwordReset(){
+        return "/user/account/password_Reset";
     }
 }

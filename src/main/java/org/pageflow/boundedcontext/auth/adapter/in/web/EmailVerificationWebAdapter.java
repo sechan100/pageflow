7238a1e54bd59d34595a1ae61a82b4024629eb96
sechan100 <sechan100@gmail.com>
2024-04-29 17:05:34 +0900

    feat: 이메일 인증 로직 web adapter 메소드 작성

--- src/main/java/org/pageflow/boundedcontext/auth/adapter/in/web/EmailVerificationWebAdapter.java
+++ src/main/java/org/pageflow/boundedcontext/auth/adapter/in/web/EmailVerificationWebAdapter.java
@@ -3,24 +3,39 @@ package org.pageflow.boundedcontext.auth.adapter.in.web;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.pageflow.boundedcontext.auth.port.in.EmailVerificationUseCase;
+import org.pageflow.boundedcontext.common.value.UID;
+import org.pageflow.global.api.ApiAccess;
 import org.pageflow.global.api.RequestContext;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.UUID;
 
 /**
  * @author : sechan
  */
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class EmailVerificationWebAdapter {
     private final RequestContext requestContext;
     private final EmailVerificationUseCase useCase;
 
 
-    public static final String EMAIL_VERIFICATION_URI = "/user/email/send-verification-email";
+    @Secured(ApiAccess.USER)
     @Operation(summary = "이메일 인증요청 전송", description = "로그인 중인 사용자의 이메일로 인증메일을 전송한다.")
-    @PostMapping(EMAIL_VERIFICATION_URI)
+    @PostMapping("/auth/email/send-verification-email")
+    @ResponseBody
     public void sendVerificationEmail() {
         useCase.sendVerificationEmail(requestContext.getUid());
+    }
+
+    public static final String EMAIL_VERIFICATION_URI = "/auth/email/verify";
+    @RequestMapping(EMAIL_VERIFICATION_URI)
+    public String verifyEmail(String uid, String authCode) {
+        useCase.verify(UID.from(uid), UUID.fromString(authCode));
+        return "/user/email-verification-success";
     }
 }

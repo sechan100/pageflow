55930c64f0f751827f232837e94ae568b9b87ab6
sechan100 <sechan100@gmail.com>
2025-03-14 11:54:28 +0900

    feat: ServerPropertiesRes에 username 필드 관련 정보 추가

--- core/src/main/java/org/pageflow/core/controller/ServerPropertiesRes.java
+++ core/src/main/java/org/pageflow/core/controller/ServerPropertiesRes.java
@@ -4,6 +4,7 @@ import lombok.Value;
 import org.pageflow.common.property.ApplicationProperties;
 import org.pageflow.user.domain.Password;
 import org.pageflow.user.port.in.service.PennameValidator;
+import org.pageflow.user.port.in.service.UsernameValidator;
 
 /**
  * @author : sechan
@@ -24,17 +25,24 @@ public class ServerPropertiesRes {
     // refresh token 만료 기간
     int refreshTokenExpireDays;
 
-    // 필명 관련
-    int pennameMinLength = PennameValidator.MIN_LENGTH;
-    int pennameMaxLength = PennameValidator.MAX_LENGTH;
-    String pennameRegex = PennameValidator.REGEX;
-    String pennameRegexMessage = PennameValidator.REGEX_MESSAGE;
+    // 아이디 관련
+    int usernameMinLength = UsernameValidator.MIN_LENGTH;
+    int usernameMaxLength = UsernameValidator.MAX_LENGTH;
+    String usernameRegex = UsernameValidator.REGEX;
+    String usernameRegexMessage = UsernameValidator.REGEX_MESSAGE;
 
     // 비밀번호 관련
     int passwordMinLength = Password.MIN_LENGTH;
     int passwordMaxLength = Password.MAX_LENGTH;
     String passwordRegex = Password.REGEX;
     String passwordRegexMessage = Password.REGEX_MESSAGE;
+
+    // 필명 관련
+    int pennameMinLength = PennameValidator.MIN_LENGTH;
+    int pennameMaxLength = PennameValidator.MAX_LENGTH;
+    String pennameRegex = PennameValidator.REGEX;
+    String pennameRegexMessage = PennameValidator.REGEX_MESSAGE;
+
   }
 }
 
--- user/src/main/java/org/pageflow/user/port/in/service/UsernameValidator.java
+++ user/src/main/java/org/pageflow/user/port/in/service/UsernameValidator.java
@@ -20,21 +20,21 @@ import java.util.Set;
 @Transactional
 @RequiredArgsConstructor
 public class UsernameValidator {
-  private static final int MIN_LENGTH = 4;
-  private static final int MAX_LENGTH = 100;
-  private static final String REGEX = "^[a-zA-Z0-9-]{" + MIN_LENGTH + "," + MAX_LENGTH + "}$";
-  private static final Collection<String> INVALID_USERNAME = Set.of(
+  public static final int MIN_LENGTH = 4;
+  public static final int MAX_LENGTH = 100;
+  public static final String REGEX = "^[a-zA-Z0-9-]{" + MIN_LENGTH + "," + MAX_LENGTH + "}$";
+  public static final String REGEX_MESSAGE = "아이디는 영문, 숫자를 사용해서 입력해주세요.";
+  public static final Collection<String> INVALID_USERNAME = Set.of(
     "admin", "administrator", "anonymous", "pageflow", "guest"
   );
 
   private final AccountPersistencePort accountPersistencePort;
 
   public FieldValidationResult validate(String username) {
-
     FieldValidator<String> validator = new FieldValidator<>("username", username)
       .minLength(MIN_LENGTH, "아이디는 최소 " + MIN_LENGTH + "자 이상이어야 합니다.")
       .maxLength(MAX_LENGTH, "아이디는 최대 " + MAX_LENGTH + "자 이하여야 합니다.")
-      .regex(REGEX, "아이디는 영문, 숫자를 사용해서 입력해주세요.")
+      .regex(REGEX, REGEX_MESSAGE)
       .notSame(INVALID_USERNAME, w -> String.format("'%s'은(는) 사용할 수 없는 아이디입니다.", w))
       .rule(u -> !accountPersistencePort.existsByUsername(u), FieldReason.DUPLICATED, "이미 사용중인 아이디입니다.");
 
